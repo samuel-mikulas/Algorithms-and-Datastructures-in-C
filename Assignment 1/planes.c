578b8a99cb03b6ebb804a7566577fdd4a5b30d32
Samuel <133594386+samuel-mikulas@users.noreply.github.com>
2025-02-06 09:31:38 +0100

    Add files via upload

--- Assignment 1/planes.c
+++ Assignment 1/planes.c
@@ -1,7 +1,7 @@
-/* file:    */
-/* author:  */
-/* date:    */
-/* version: */
+/* file :    planes.c */
+/* author :  Samuel Mikulas (s.mikulas@student.rug.nl) */
+/* date :    Tue 4 Feb 2024 */
+/* version:  1.0 */
 
 /* Description:
     Program takes n planes and a status for repair of each plain (y/n)
@@ -18,11 +18,18 @@
 void clearRunway(Queue *runway);
 void clearHangair(Stack *hangair);
 
+#define RUNWAY_CAPACITY  7
+#define HANGAIR_CAPACITY 5
+
 int main(int argc, char *argv[]) {
   
   int NumberOfPlanes;
-  Queue runway = newQueue(7);
-  Stack hangar = newStack(5);
+  Queue runway = newQueue(RUNWAY_CAPACITY);
+  Stack hangar = newStack(HANGAIR_CAPACITY);
+
+  // Keep track of planes on runway and hangair
+  int runwaySize = 0;
+  int hangarSize = 0;
 
   scanf("%d", &NumberOfPlanes);
   for (int i = 0; i < NumberOfPlanes; i++){
@@ -33,20 +40,24 @@ int main(int argc, char *argv[]) {
     if (status[0] == 'n'){
       // Ready to leave
       enqueue(curPlane, &runway);
+      runwaySize++;
     } else {
       // Needs repair
       push(curPlane, &hangar);
+      hangarSize++;
     }
 
     // Hangar reached its full capacity
-    if (hangar.size == hangar.top){
+    if (hangarSize == HANGAIR_CAPACITY){
       clearRunway(&runway);
       clearHangair(&hangar);
+      hangarSize = runwaySize = 0;
     }
 
     // Runway reached its full capacity
-    if (runway.front == runway.size){
+    if (runwaySize == RUNWAY_CAPACITY){
       clearRunway(&runway);
+      runwaySize = 0;
     }
   }
 
@@ -61,14 +72,14 @@ int main(int argc, char *argv[]) {
 
 // Function empties queue and outputs every plane
 void clearRunway(Queue *runway){
-  while (runway->front != runway->back){
+  while (!isEmptyQueue(*runway)){
     printf("%d\n", dequeue(runway));
   }
 }
 
 // Function empties stack and outputs every plane
 void clearHangair(Stack *hangair){
-  while (hangair->top > 0){
+  while (!isEmptyStack(*hangair)){
     printf("%d\n", pop(hangair));
   }
 } 
