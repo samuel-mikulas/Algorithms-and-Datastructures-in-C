ca6cad17476d1d1637d80427fb5a58fc0daf2495
Samuel <133594386+samuel-mikulas@users.noreply.github.com>
2025-02-21 17:30:06 +0100

    Create mainEvalEq.c

---
+++ Assignment 2/Bonus 1/mainEvalEq.c
@@ -0,0 +1,12 @@
+#include <stdlib.h> 
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+#include "scanner.h"
+#include "recognizeEq.h"
+
+int main(int argc, char *argv[]) {
+  recognizeEquation();
+  return 0;
+}
